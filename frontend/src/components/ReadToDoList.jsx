e937969fcb52b2c3488a234eea6f739b6214fc13
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-09 01:02:40 -0600

    Add search and filter

--- frontend/src/components/ReadToDoList.jsx
+++ frontend/src/components/ReadToDoList.jsx
@@ -5,8 +5,27 @@ import { Link } from "react-router-dom";
 
 // component for getting to do list
 const ReadToDoList = () => {
-  // state to store to do list
+  // state variables
+  const [categories, setCategories] = useState([]);
   const [toDoList, setToDoList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [categoryFilter, setCategoryFilter] = useState("All");
+  const [sortByDueDateAsc, setSortByDueDateAsc] = useState(true);
+  const [filteredToDoList, setFilteredToDoList] = useState([]);
+
+  // get and set categories from server
+  const readCategories = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/get-categories");
+
+      if (res.status === 200) {
+        // set categories in state
+        setCategories(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // get and set to do list from server
   const readToDoList = async () => {
@@ -25,7 +44,39 @@ const ReadToDoList = () => {
   // run readToDoList on component mount
   useEffect(() => {
     readToDoList();
+    readCategories();
   }, []);
+
+  // run applySearchAndFilters on state variable changes
+  useEffect(() => {
+    applySearchAndFilters();
+  }, [toDoList, categoryFilter, searchText, sortByDueDateAsc]);
+
+  // apply search and filters
+  const applySearchAndFilters = () => {
+    let filteredList = toDoList.filter((item) => {
+      const { title, description } = item;
+      return (
+        title.toLowerCase().includes(searchText.toLowerCase()) ||
+        description.toLowerCase().includes(searchText.toLowerCase())
+      );
+    });
+
+    filteredList = filteredList.filter((item) => {
+      if (categoryFilter !== "All" && item.category) {
+        return item.category.name === categoryFilter;
+      }
+      return true;
+    });
+
+    filteredList.sort((a, b) => {
+      const dateA = new Date(a.dueDate);
+      const dateB = new Date(b.dueDate);
+      return sortByDueDateAsc ? dateA - dateB : dateB - dateA;
+    });
+
+    setFilteredToDoList(filteredList);
+  };
 
   // function to format date to MM/DD/YYYY HH:MM
   const formatDate = (dateString) => {
@@ -51,6 +102,12 @@ const ReadToDoList = () => {
 
     // return the formatted date
     return formattedDate;
+  };
+
+  const clearFilters = () => {
+    setSearchText("");
+    setCategoryFilter("All");
+    setSortByDueDateAsc(true);
   };
 
   const moveToBin = async (taskId) => {
@@ -79,15 +136,67 @@ const ReadToDoList = () => {
 
   // JSX for the component
   return (
-    <div>
+    <div className="container">
+      {/* search bar */}
+      <input
+        type="text"
+        placeholder="Search by Title or Description"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="form-control mb-3"
+      />
+
+      <div className="row">
+        <div className="col">
+          {/* category filter */}
+          <select
+            id="categoryFilter"
+            value={categoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)}
+            className="form-select mb-3"
+          >
+            <option value="All">All</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.name}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="col">
+          {/* due date sort by button */}
+          <button
+            id="sortByDueDate"
+            onClick={() => setSortByDueDateAsc(!sortByDueDateAsc)}
+            className="btn btn-primary mb-3"
+          >
+            {sortByDueDateAsc ? "Ascending" : "Descending"}
+          </button>
+        </div>
+
+        <div className="col">
+          {/* button to clear all filters */}
+          <button onClick={clearFilters} className="btn btn-secondary mb-3">
+            Clear Filters
+          </button>
+        </div>
+      </div>
+
+      {/* horizontal line */}
+      <hr />
+
       <h2>To Do List</h2>
+
       {/* link to navigate to Add To Do page */}
-      <Link to="/add" className="btn btn-primary-task">
-        Add Task
-      </Link>
+      <div className="add-button">
+        <Link to="/add" className="btn btn-primary">
+          Add Task
+        </Link>
+      </div>
 
       {/* check if to do list exists */}
-      {toDoList && toDoList.length > 0 ? (
+      {filteredToDoList && filteredToDoList.length > 0 ? (
         // display table with to do list
         <table className="custom-table">
           {/* table column titles */}
@@ -114,7 +223,7 @@ const ReadToDoList = () => {
           {/* table rows */}
           <tbody>
             {/* map through to do list and display each item in table row */}
-            {toDoList.map((toDo) => (
+            {filteredToDoList.map((toDo) => (
               <tr key={toDo._id}>
                 <td
                   className={`text-center custom-category-${
